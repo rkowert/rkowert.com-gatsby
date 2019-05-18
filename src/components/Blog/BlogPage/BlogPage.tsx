edc17caeded6642eb7b30c5b2bfe69fb3a5511de
Rex McConnell <rex@rexmac.com>
2019-05-18 16:30:42 -0400

    ✨ Add Things I Love to blog sidebar

--- src/components/Blog/BlogPage/BlogPage.tsx
+++ src/components/Blog/BlogPage/BlogPage.tsx
@@ -21,11 +21,36 @@ const BlogSidebar = styled.div`
   }
 `;
 
+const ThingsILove = styled.ul`
+  > li {
+    margin-bottom: 0;
+  }
+`;
+
 export default function({ children }) {
   return (
     <BlogPage>
       {children}
       <BlogSidebar>
+        <h3>Things I Love</h3>
+        <ThingsILove>
+          <li>Pragmatic Princess</li>
+          <li>
+            <a href="http://www.takethis.org/">TakeThis</a>
+          </li>
+          <li>
+            <a href="http://stats.stackexchange.com/">Cross Validated</a>
+          </li>
+          <li>
+            <a href="http://www.phdcomics.com/">PhD Comics</a>
+          </li>
+          <li>
+            <a href="http://geekandsundry.com/">Geek and Sundry</a>
+          </li>
+          <li>
+            <a href="http://rexmac.com/mturk">MTurk-Qualtrics Helper</a>
+          </li>
+        </ThingsILove>
         <h3>Tag Cloud</h3>
         <TagCloud />
       </BlogSidebar>
